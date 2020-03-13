512dd8068fd5b6e5c8ca44f8545783302dde66e6
Ivan Shavliuga <ivanov-R0M@yandex.ru>
2020-03-13 14:49:25 +0300

    new field user

--- bulletin-board-app/backend/events.js
+++ bulletin-board-app/backend/events.js
@@ -3,17 +3,21 @@ module.exports = [
       id: 1,
       title: 'Docker Workshop',
       detail: 'Linuxing in Lonodon ',
-      date: '2017-11-21'
+      date: '2017-11-21',
+      user: 'iv2'
     },
     {
       id: 2,
       title: 'WinOps #17',
       detail: 'WinOps London',
-      date: '2017-11-21'
+      date: '2017-11-21',
+      user:'ann24'
     },
     {
       id: 3,
       title: 'Docker London',
-      date: '2017-11-13'
+      detail:"Text",
+      date: '2017-11-13',
+      user: 'sys32'
     }
   ];
